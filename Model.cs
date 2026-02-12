81cec9942d2cc3684c303973db8a1840ccd85623
LucaN <118000599+Zycloak@users.noreply.github.com>
2026-02-12 12:03:23 +0100

    datamodel tasks

---
+++ Model.cs
@@ -0,0 +1,12 @@
+public class Task 
+{ 
+    public int Id { get; set; } 
+    public string Name { get; set; } 
+    public bool Status { get; set; } 
+    public Task(int id, string name, bool status) 
+    { 
+        Id = id; 
+        Name = name; 
+        Status = status; 
+    } 
+}
