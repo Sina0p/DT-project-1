144959e3432861a0e8385fdbff5e4f2982946dca
Sina0p <sinahashemi134@gmail.com>
2026-02-12 19:22:43 +0100

    while loop toegevoegd aan to do list methode zodat de console niet stopt na 1 taak

--- View.cs
+++ View.cs
@@ -4,6 +4,7 @@ public class View
 {
     private Service service;
     public List<Task> tasks = new List<Task>();
+    public bool exit = false;
     
     public void DisplayTasks()
     {
@@ -18,55 +19,60 @@ public class View
             }
         }
     }
+
     public void to_do_list()
     {
-        Console.WriteLine("1. Add Task");
-        Console.WriteLine("2. Remove Task");
-        Console.WriteLine("3. Toggle Task State");
-        Console.WriteLine("4. Exit");
-        string Answer_for_to_do = Console.ReadLine();
+        while(!exit)
+        {
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. Remove Task");
+            Console.WriteLine("3. Toggle Task State");
+            Console.WriteLine("4. Exit");
+            string Answer_for_to_do = Console.ReadLine();
 
-        if (Answer_for_to_do == "4")
-        {
-            Console.WriteLine("ok 4");
-            //code hadnling voor 4
-        }
-        else if (Answer_for_to_do == "3")
-        {
-            Console.Clear();
-            DisplayTasks();
-            Console.WriteLine("\n Type task integer to toggle a task");
-            service.ToggleTask(int.Parse(Console.ReadLine()));
-            //code hadnling voor 3
-        }
-        else if (Answer_for_to_do == "2")
-        {
-            Console.WriteLine("What do you want to delete");
-            var delete = new Service(tasks);
-            string Task_to_delete = Console.ReadLine();
-            if(delete.DeleteTask(Task_to_delete))
+            if (Answer_for_to_do == "4")
             {
-                Console.WriteLine($"You have deleted {Task_to_delete}");
+                exit = true;
             }
-            Console.WriteLine("This task does not exist");
-        }
-        else if (Answer_for_to_do == "1")
-        {
-            Console.WriteLine("What would yo like to add");
-            var add = new Service(tasks);
-            string Task_to_add = Console.ReadLine();
-            if(add.AddTask(Task_to_add))
+            else if (Answer_for_to_do == "3")
             {
-                Console.WriteLine($"You have added {Task_to_add}");
-                return;
+                Console.Clear();
+                DisplayTasks();
+                Console.WriteLine("\n Type task integer to toggle a task");
+                service.ToggleTask(int.Parse(Console.ReadLine()));
+                //code hadnling voor 3
             }
-            Console.WriteLine("No space left to add a task");
-            return;
-        }
-        else
-        {
-            Console.WriteLine("wrong input");
-            //code hadnling voor verkeerde input
+            else if (Answer_for_to_do == "2")
+            {
+                Console.WriteLine("What do you want to delete");
+                var delete = new Service(tasks);
+                string Task_to_delete = Console.ReadLine();
+                if(delete.DeleteTask(Task_to_delete))
+                {
+                    Console.WriteLine($"You have deleted {Task_to_delete}");
+                    continue;
+                }
+                Console.WriteLine("This task does not exist");
+                continue;
+            }
+            else if (Answer_for_to_do == "1")
+            {
+                Console.WriteLine("What would yo like to add");
+                var add = new Service(tasks);
+                string Task_to_add = Console.ReadLine();
+                if(add.AddTask(Task_to_add))
+                {
+                    Console.WriteLine($"You have added {Task_to_add}");
+                    continue;
+                }
+                Console.WriteLine("No space left to add a task");
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("wrong input");
+                //code hadnling voor verkeerde input
+            }
         }
     }
 }
--- obj/Debug/net10.0/DT-project-1.AssemblyInfo.cs
+++ obj/Debug/net10.0/DT-project-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DT-project-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a607517bc3612b00db4575d043d1a447b75fb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec542b3a416fc1f5f465bb06a27de53467b86448")]
 [assembly: System.Reflection.AssemblyProductAttribute("DT-project-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DT-project-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
