d25468348dd9ce05875f5479a46a444c2319f332
Sina0p <sinahashemi134@gmail.com>
2026-02-12 11:51:29 +0100

    alle opties gegeven voor to_do_list en basis handling

--- View.cs
+++ View.cs
@@ -2,8 +2,20 @@ public class View
 {
     public void to_do_list()
     {
-        Console.WriteLine("Add");
-    }
+        Console.WriteLine("1. Add Task");
+        Console.WriteLine("2. Remove Task");
+        Console.WriteLine("3. Toggle Task State");
+        Console.WriteLine("4. Exit");
+        string Answer_for_to_do = Console.ReadLine();
 
+        if (Answer_for_to_do == "4")
+        {
+            Console.WriteLine("ok 4");
+        }
+        else
+        {
+            Console.WriteLine("ok ");
+        }
+    }
 }
 
