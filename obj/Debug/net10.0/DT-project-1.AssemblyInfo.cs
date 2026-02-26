245abd0837fa620d1653b0d50378877c47ee933f
QXOonincx <lakeyfn.70@gmail.com>
2026-02-26 13:42:33 +0100

    je kan tasks deleten uit de jsn

--- obj/Debug/net10.0/DT-project-1.AssemblyInfo.cs
+++ obj/Debug/net10.0/DT-project-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DT-project-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec542b3a416fc1f5f465bb06a27de53467b86448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24d7ae4ccaf9e841c388476580da6a6216c6c7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DT-project-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DT-project-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
