5aff7b75c8015b8d60de3487861c6eb1c7be887c
Sina0p <sinahashemi134@gmail.com>
2026-04-04 21:31:40 +0200

    made an if statement that handles empty json file

--- Repository.cs
+++ Repository.cs
@@ -1,12 +1,18 @@
-
 using System.Text.Json;
 public class Repository
 {
     public List<Task> loadTask()
     {
         var json = File.ReadAllText("data.json");
-        List<Task> tasks = JsonSerializer.Deserialize<List<Task>>(json)!;
-        return tasks;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return new List<Task>();
+        }
+
+        List<Task>? tasks = JsonSerializer.Deserialize<List<Task>>(json);
+
+        return tasks ?? new List<Task>();
     }
 
     public void saveTask(List<Task> tasks)
